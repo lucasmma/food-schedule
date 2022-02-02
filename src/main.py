0554350bc75e9fee97ff85a10a95c89fdeed9527
Lucas Mendonça <lucasmmamaral@gmail.com>
2022-02-02 18:27:20 -0300

    feat: add register e list function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, Response, redirect, url_for
+from infra import append_data_on_database, read_database
+import traceback
+from datetime import date
 
 app = Flask(__name__, template_folder='templates')
 
+database = 'database.json'
 
 def register_meal():
   print("ola")
@@ -9,25 +13,30 @@ def register_meal():
 
 @app.route("/")
 def main():
-  return render_template('index.html', foods=get_recent_meals())
+  return render_template('index.html', foods=get_recent_meals(), date= date.today().strftime("%d/%m/%Y"))
 
 
 @app.route("/recentMeals", methods=["GET"])
 def get_recent_meals():
-  print('ola1')
   try:
-    return 'ola' 
+    return read_database()
   except Exception:
-      print("exception")
+    print(traceback.format_exc())
+    return 'erro'
 
 @app.route("/registerMeal", methods=["POST"])
 def register_meal():
-  print('registrado')
-  print(request.form['food'])
   try:
-    return 'ola' 
-  except Exception:
-      print("exception")
+    food = request.form['food']
+    schedule = request.form['schedule']
+    data = {
+      'food': food,
+      'schedule': schedule,
+    }
+    append_data_on_database(data)
+    return data
+  except Exception as err:
+    print(traceback.format_exc())
 
 if __name__ == "__main__":
-    app.run(debug=True, port=3000)
+    app.run(debug=True, port=3000, host='0.0.0.0')
